f55b98d5861d7e2b0b641a2128092f6a6d6c6c65
redli <redli@DESKTOP-LJJA4GR>
2023-09-13 22:23:20 +0300

    Load

---
+++ 1 ex/1 ex.cpp
@@ -0,0 +1,16 @@
+﻿// 1 ex.cpp: определяет точку входа для приложения.
+//
+
+#include "1 ex.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string fname;
+	cout << "Введите фамилию: ";
+	cin >> fname;
+	cout << "Ваш вариант: " << fname.length() % 2 + 1;
+	return 0;
+}
