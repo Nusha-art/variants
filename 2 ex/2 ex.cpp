d765fc1724c3ae0f7b9483d244837ea8c1328bc6
redli <redli@DESKTOP-LJJA4GR>
2023-09-18 20:44:21 +0300

    2 ex

---
+++ 2 ex/2 ex.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class FooString {
+    char* buf;
+public:
+    FooString(char* tbuf) {
+        buf = tbuf;
+    };
+    ~FooString() {
+    };
+    void show() {
+        std::cout << buf << '\n';
+    };
+    bool compare(char* str) {
+        if (str == buf) {
+            cout << "true";
+            return true;
+        }
+        else return false;
+    }
+};
+
+int main()
+{
+    const char* myBuf = "This is a const char";
+    char* myB = const_cast<char*>(myBuf);
+    const char* myString = "This is a const char";
+    char* myChar = const_cast<char*>(myString);
+    FooString foo= myB;
+    foo.compare(myChar);
+}
